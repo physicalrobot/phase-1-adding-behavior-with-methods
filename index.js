6c9937bcc0fc5243e314c9bb4d94d34b5e87aa00
Vikalp Malhotra <kimboho@gmail.com>
2021-12-31 13:37:13 -0700

    finished

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() { return `${this.name} says meow!` }
+}
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() { return `${this.name} says woof!` }
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        if (this.sex == "male") {
+            return `It's me! ${this.name}, the parrot!`
+        } else {
+            return `${this.name} says squawk!`
+        }
+    }
+}
+
+
+// if (this.sex == 'male') { `It's me! ${this.name}, the parrot!` } else { `${this.name} says squawk!` } 
